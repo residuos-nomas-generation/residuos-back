6e9a78e543cb747a2eb495d408c42daab0373618
rigozdev <rodrigo.gonzaleztr@outlook.com>
2024-02-12 14:41:07 -0300

    models

--- src/main/java/com/generation/backendproject/model/VigenciaPublicacion.java
+++ src/main/java/com/generation/backendproject/model/VigenciaPublicacion.java
@@ -1,4 +1,18 @@
 package com.generation.backendproject.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Entity
+@Data
+
 public class VigenciaPublicacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "vigencia_publicacion")
+    private Long vigenciaPublicacion;
+
+    @Column(name = "descripcion_vigencia", nullable = false, length = 100)
+    private String descripcionVigencia;
 }
